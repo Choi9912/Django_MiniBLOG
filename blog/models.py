d6c1b40762c9767aa5febc00f91b0a242c2b0151
Choi9912 <vkvkdl23@naver.com>
2024-08-20 10:48:58 +0900

    insta느낌으로 구성

--- blog/models.py
+++ blog/models.py
@@ -38,10 +38,22 @@ class Tag(models.Model):
         return f"/blog/tag/{self.slug}/"
 
 
+from django.db import models
+from django.contrib.auth.models import User
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
+    head_image = ProcessedImageField(
+        upload_to="blog/images/%Y/%m/%d/",
+        processors=[ResizeToFill(300, 300)],
+        format="JPEG",
+        options={"quality": 60},
+        blank=True,
+    )
     file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d/", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -52,6 +64,9 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     view_count = models.PositiveIntegerField(default=0)
     likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     "bootstrap4",
     "blog",
     "widget_tweaks",
+    "imagekit",
 ]
 
 MIDDLEWARE = [
